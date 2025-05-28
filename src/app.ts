a88edad4cb9ef320af9197c4d0df7981c6d68f62
Neri Laredo <nerila1310@gmail.com>
2025-05-28 12:09:45 -0600

    Configurar variables de entorno

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,16 @@
+import { envs } from "./config/envs";
 import { Server } from "./presentation/server";
 
-(() => {
+(async () => {
   main();
-})()
+})();
 
 function main() {
   // console.log('Hello, World from main function!');
 
-  const server = new Server();
+  const server = new Server({
+    port: envs.PORT,
+    public_path: envs.PUBLIC_PATH
+  });
   server.start();
 }
--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,8 +1,21 @@
 import express from "express";
 import path from "path";
 
+interface Options {
+  port: number;
+  public_path?: string;
+}
+
 export class Server {
   private app = express();
+  private readonly port: number;
+  private readonly publicPath: string;
+
+  constructor(options: Options) {
+    const { port, public_path = "public" } = options;
+    this.port = port;
+    this.publicPath = public_path;
+  }
 
   async start() {
     console.log("Server started...");
@@ -13,12 +26,15 @@ export class Server {
     this.app.use(express.static("./public"));
 
     this.app.get("/{*splat}", (req, res) => {
-      const indexPath = path.join(__dirname, "../../public/index.html");
+      const indexPath = path.join(
+        __dirname,
+        `../../${this.publicPath}/index.html`
+      );
       res.sendFile(indexPath);
     });
 
-    this.app.listen(3000, () => {
-      console.log(`Server is running on port ${3000}`);
+    this.app.listen(this.port, () => {
+      console.log(`Server is running on port ${this.port}`);
     });
   }
 }
